08c7e0c352f3b6df4af1b0b5edeb863a1b68e65b
Anvesh Singh <125560114+anvesh-s@users.noreply.github.com>
2023-06-27 16:34:21 +0530

    Add files via upload
    Components are used to write re-usable code which can be used at different points in a React project. 
    
    Created using JSX.

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Heading from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import Note from "./Note";
+
+
+
+function App() {
+    return (
+    <div>
+        <Heading />
+        <Note />
+        <Footer />
+
+    </div>
+    );
+}
+
+export default App;
